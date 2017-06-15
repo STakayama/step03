ad859badd1fc7cf188ca5cf64615f9d14842a9cc
高山沙也加 <sayaka@takayamasayaka-no-MacBook-Pro.local>
2017-06-15 09:53:19 +0900

    コード整え中

--- calculator2.py
+++ calculator2.py
@@ -72,7 +72,7 @@ def tokenize(line):
 
 def evaluate_par(tokens):# evaluate ( including its internal OK
     new_tokens=[]
-    zero_index=1
+    index=1
     tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
     t_index=1
     while t_index < len(tokens):
@@ -80,9 +80,9 @@ def evaluate_par(tokens):# evaluate ( including its internal OK
             if tokens[t_index-1]['type'] == 'PAR_F':
 
                 if t_index>1:#*( , /( etc use read... other def
-                    (token,index)=({'type':tokens[t_index-2]['type']},zero_index)
+                    (token,index)=({'type':tokens[t_index-2]['type']},index)
                     new_tokens.append(token)
-                    zero_index += 1
+                    index += 1
 
                 par_tokens=[]
                 par_f_num=1
@@ -108,33 +108,33 @@ def evaluate_par(tokens):# evaluate ( including its internal OK
                     t_index+=1
                 par_l_index=t_index-1
                 par_result=evaluate(par_tokens)#ok
-                (token,index)=({'type':'NUMBER','number':par_result},zero_index) 
-                zero_index+=1
+                (token,index)=({'type':'NUMBER','number':par_result},index) 
+                index+=1
                 
             elif tokens[t_index - 1]['type'] == 'MULTIPLY':
-                (token,index)=readMul(tokens,zero_index-1)
+                (token,index)=readMul(tokens,index-1)
                 new_tokens.append(token)
-                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},zero_index)
-                zero_index += 1
+                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},index)
+                index += 1
             
                         
             elif tokens[t_index - 1]['type'] == 'DIVIDE':
-                (token,index)=readDiv(tokens,zero_index-1)
+                (token,index)=readDiv(tokens,index-1)
                 new_tokens.append(token)
-                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},zero_index)
-                zero_index += 1
+                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},index)
+                index += 1
 
             elif tokens[t_index - 1]['type'] == 'PLUS':
-                (token,index)=readPlus(tokens,zero_index-1)
+                (token,index)=readPlus(tokens,index-1)
                 new_tokens.append(token)
-                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},zero_index)
-                zero_index += 1
+                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},index)
+                index += 1
             
             elif tokens[t_index - 1]['type'] == 'MINUS':
-                (token,index)=readMinus(tokens,zero_index-1)
+                (token,index)=readMinus(tokens,index-1)
                 new_tokens.append(token)
-                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},zero_index)
-                zero_index += 1                
+                (token,index)=({'type':'NUMBER','number':tokens[t_index]['number']},index)
+                index += 1                
                 
             else:
                 print 'Invalid syntax'
